f85f69b38bab9f666518c686fdc3fba51b01967a
rosenrose <crazytempler@gmail.com>
2022-09-20 17:50:50 +0900

    EmotionJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React, { FunctionComponent } from "react";
 import { Link } from "gatsby";
-import Text from "../components/Text";
 
 const IndexPage: FunctionComponent = () => {
   return (
     <>
-      <Text text="Home" />
       <Link to="/info">To Info</Link>
     </>
   );
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { graphql, Link } from "gatsby";
-import Text from "../components/Text";
+import { Global, css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 type InfoPageProps = {
   data: {
@@ -26,6 +27,35 @@ export const metadataQuery = graphql`
   }
 `;
 
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const textStyle = css`
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: skyblue;
+`;
+
+interface ITextProp {
+  disabled?: boolean;
+}
+
+const Text1 = styled.div<ITextProp>`
+  font-size: 1.5rem;
+  color: seagreen;
+  text-decoration: ${(props) => (props.disabled ? "line-through" : "")};
+`;
+const Text2 = styled.div<ITextProp>((props) => ({
+  fontSize: "1.8rem",
+  color: "cyan",
+  textDecoration: props.disabled ? "line-through" : "",
+}));
+
 const InfoPage: FunctionComponent<InfoPageProps> = ({
   data: {
     site: {
@@ -35,9 +65,11 @@ const InfoPage: FunctionComponent<InfoPageProps> = ({
 }) => {
   return (
     <>
-      <Text text={description} />
-      <Text text={siteUrl} />
-      <Text text={title} />
+      <Global styles={globalStyle} />
+      <p>{description}</p>
+      <div css={textStyle}>{siteUrl}</div>
+      <Text1>{title}</Text1>
+      <Text2 disabled>{title}</Text2>
       <Link to="/">To Home</Link>
     </>
   );
